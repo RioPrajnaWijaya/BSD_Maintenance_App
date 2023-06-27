fcaf0454cfafa92e032598f4d4bd4654d64e9ca2
kennn21 <91940623+kennn21@users.noreply.github.com>
2023-06-27 11:00:21 +0700

    Test

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   Map<String, dynamic> user = {};
   Map<String, dynamic> doctor = {};
   List<dynamic> favList = [];
@@ -55,112 +54,84 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     Config().init(context);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 15
-        ),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Acil',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold
-                      )
-                    ),
-                    SizedBox(
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('assets/kenn.jpg'),
-                      ),
-                    )
-                  ]
-                ),
-                Config.spaceSmall,
-                const Text(
-                  'Category',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  )
-                ),
-                Config.spaceSmall,
-                SizedBox(
-                  height: Config.heightSize * 0.05,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: List<Widget>.generate(medCat.length, (index) {
-                      return Card(
-                        margin: const EdgeInsets.only(right: 20),
-                        color: Config.primaryColor,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 10
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: SafeArea(
+              child: SingleChildScrollView(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('Test',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold)),
+                        SizedBox(
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage: AssetImage('assets/kenn.jpg'),
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              FaIcon(
-                                medCat[index]['icon'],
-                                color: Colors.white,
-                              ),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              Text(
-                                medCat[index]['category'],
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white
-                                ),
-                              )
-                            ],
-                          )
                         )
+                      ]),
+                  Config.spaceSmall,
+                  const Text('Category',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Config.spaceSmall,
+                  SizedBox(
+                      height: Config.heightSize * 0.05,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: List<Widget>.generate(medCat.length, (index) {
+                          return Card(
+                              margin: const EdgeInsets.only(right: 20),
+                              color: Config.primaryColor,
+                              child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: <Widget>[
+                                      FaIcon(
+                                        medCat[index]['icon'],
+                                        color: Colors.white,
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Text(
+                                        medCat[index]['category'],
+                                        style: const TextStyle(
+                                            fontSize: 16, color: Colors.white),
+                                      )
+                                    ],
+                                  )));
+                        }),
+                      )),
+                  Config.spaceSmall,
+                  const Text('Available Today',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Config.spaceSmall,
+                  const MaintenanceCard(),
+                  Config.spaceSmall,
+                  const Text(
+                    'Top Staff',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Config.spaceSmall,
+                  Column(
+                    children: List.generate(10, (index) {
+                      return const StaffCard(
+                        route: 'staff_details',
                       );
                     }),
                   )
-                ),
-                Config.spaceSmall,
-                const Text(
-                  'Available Today',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  )
-                ),
-                Config.spaceSmall,
-                const MaintenanceCard(),
-                Config.spaceSmall,
-                const Text(
-                  'Top Staff',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                Config.spaceSmall,
-                Column(
-                  children: List.generate(10, (index) {
-                    return const StaffCard(
-                      route: 'staff_details',
-                    );
-                  }),
-                )
-              ],
-            )
-          ),
-        )
-      )
-    );
+                ],
+              )),
+            )));
   }
 }
